4b3435120416bb1c13b45d557f0c8fac5dbe2313
Awis A <awis0330@gmail.com>
2017-05-04 01:35:58 +0800

    update react native project skeleton

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class powerline extends Component {
+export default class Powerline extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('powerline', () => powerline);
+AppRegistry.registerComponent('Powerline', () => Powerline);
--- ios/powerline/AppDelegate.m
+++ ios/powerline/AppDelegate.m
@@ -21,7 +21,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"powerline"
+                                                      moduleName:@"Powerline"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
