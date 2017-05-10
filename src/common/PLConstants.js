59ae7c41af52f3fa27cf9005b011c0df5c0e5898
Awis A <awis0330@gmail.com>
2017-05-10 22:04:44 +0800

    issue #2 - add global module to store constants in app

---
+++ src/common/PLConstants.js
@@ -0,0 +1,10 @@
+/**
+ * @providesModule PLConstants
+ **/
+
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export const WINDOW_WIDTH = width;
+export const WINDOW_HEIGHT = height;
