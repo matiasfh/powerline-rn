db72419d718d6c830a54cc98bec090fd50469c9a
Awis A <awis0330@gmail.com>
2017-05-18 10:44:22 +0800

    issue #3 - Android: configure Facebook

--- android/app/src/main/java/com/powerline/MainApplication.java
+++ android/app/src/main/java/com/powerline/MainApplication.java
@@ -4,10 +4,14 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 
 import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
@@ -18,6 +22,12 @@ import com.BV.LinearGradient.LinearGradientPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected  static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -27,9 +37,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new SplashScreenReactPackage(),
-          new LinearGradientPackage()
+              new MainReactPackage(),
+              new FBSDKPackage(mCallbackManager),
+              new SplashScreenReactPackage(),
+              new LinearGradientPackage()
       );
     }
   };
