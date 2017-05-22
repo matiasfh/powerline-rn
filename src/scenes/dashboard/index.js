75298bd83f8e4db47f1d4ea95738ab6008e4b338
Awis A <awis0330@gmail.com>
2017-05-23 00:46:27 +0800

    issue #7 - add tab bar with icons from native base  * apply styles

---
+++ src/scenes/dashboard/index.js
@@ -0,0 +1,112 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Actions, ActionConst } from 'react-native-router-flux';
+import { Container, Header, Title, Content, Button, Footer, FooterTab, Text, Body, Left, Right, Icon } from 'native-base';
+
+import { openDrawer } from '../../actions/drawer';
+import styles from './styles';
+
+class Home extends Component {
+
+  static propTypes = {
+    openDrawer: React.PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab1: false,
+      tab2: false,
+      tab3: true,
+      tab4: false,
+    };
+  }
+
+  toggleTab1() {
+    this.setState({
+      tab1: true,
+      tab2: false,
+      tab3: false,
+      tab4: false,
+    });
+  }
+
+  toggleTab2() {
+    this.setState({
+      tab1: false,
+      tab2: true,
+      tab3: false,
+      tab4: false,
+    });
+  }
+
+  toggleTab3() {
+    this.setState({
+      tab1: false,
+      tab2: false,
+      tab3: true,
+      tab4: false,
+    });
+  }
+
+  toggleTab4() {
+    this.setState({
+      tab1: false,
+      tab2: false,
+      tab3: false,
+      tab4: true,
+    });
+  }
+
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Header style={styles.header}>
+          <Left>
+            <Button transparent onPress={this.props.openDrawer}>
+              <Icon active name="menu" style={{color: 'white'}}/>
+            </Button>
+          </Left>
+        </Header>
+
+        <Content padder />
+        
+        <Footer>
+          <FooterTab>
+            <Button active={this.state.tab1} onPress={() => this.toggleTab1()} >
+              <Icon active={this.state.tab1} name="ios-flash" />
+              <Text>Newsfeed</Text>
+            </Button>
+            <Button active={this.state.tab2} onPress={() => this.toggleTab2()} >
+              <Icon active={this.state.tab2} name="md-people" />
+              <Text>Camera</Text>
+            </Button>
+            <Button>
+              <Icon name="ios-add-circle" style={{fontSize: 40, color: '#030366'}} />
+            </Button>
+            <Button active={this.state.tab3} onPress={() => this.toggleTab3()} >
+              <Icon active={this.state.tab3} name="md-mail" />
+              <Text>Compass</Text>
+            </Button>
+            <Button active={this.state.tab4} onPress={() => this.toggleTab4()} >
+              <Icon active={this.state.tab4} name="md-notifications" />
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+function bindAction(dispatch) {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+  };
+}
+
+const mapStateToProps = state => ({
+});
+
+export default connect(mapStateToProps, bindAction)(Home);
---
+++ src/scenes/dashboard/styles.js
@@ -0,0 +1,21 @@
+
+var React = require('react-native');
+var { StyleSheet } = React;
+var PLColors = require('PLColors');
+
+export default {
+  container: {
+    backgroundColor: '#FFF',
+  },
+  text: {
+    alignSelf: 'center',
+    marginBottom: 7,
+    fontSize: 10,
+  },
+  mb: {
+    marginBottom: 15,
+  },
+  header: {
+    backgroundColor: PLColors.main,
+  },
+};
