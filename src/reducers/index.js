81635f465ca64ff4965ff3ac2c11f4f46075f71a
Awis A <awis0330@gmail.com>
2017-05-31 02:23:13 +0800

    issue #13 - add reducer for bookmarks actions  * stack items

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,9 +9,11 @@ var { combineReducers } = require('redux');
 import drawer from './drawer';
 import user from './user';
 import groups from './groups';
+import bookmarks from './bookmarks';
 
 module.exports = combineReducers({
   user,
   drawer,
   groups,
+  bookmarks,
 });
