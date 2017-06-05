719ba077d5cba4d9ca667c7a61a57ab3ac264d59
Awis A <awis0330@gmail.com>
2017-06-05 19:09:17 +0800

    issue #13 - add actions for getting activities with parameters

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,12 @@ const navigationActions = require('./navigation');
 const loginActions = require('./login');
 const groupActions = require('./groups');
 const bookmarkActions = require('./bookmarks');
+const activityActions = require('./activities');
 
 module.exports = {
   ...loginActions,
   ...navigationActions,
   ...groupActions,
   ...bookmarkActions,
+  ...activityActions,
 };
