59713615c6198f81b2a73230b9cb17208e43963b
Awis Alqarni <awis0330@gmail.com>
2017-06-09 01:50:19 +0800

    issue #13 - get youtube id from url

--- src/scenes/dashboard/newsfeed/index.js
+++ src/scenes/dashboard/newsfeed/index.js
@@ -78,6 +78,19 @@ class Newsfeed extends Component {
         }
     }
 
+    youtubeGetID(url) {
+        var ID = '';
+        url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+        if (url[2] !== undefined) {
+            ID = url[2].split(/[^0-9a-z_\-]/i);
+            ID = ID[0];
+        }
+        else {
+            ID = url;
+        }
+        return ID;
+    }
+
     _onRefresh() {
         this.props.dispatch(resetActivities());
         this.loadInitialActivities();
