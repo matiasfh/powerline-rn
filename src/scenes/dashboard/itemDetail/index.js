f6c98480eea6c5dfb307e995926b6a39a6df36ad
Awis A <awis0330@gmail.com>
2017-06-22 21:13:20 +0800

    issue #19 - replicate carousel and metadata container from newsfeed

--- src/scenes/dashboard/itemDetail/index.js
+++ src/scenes/dashboard/itemDetail/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body, Thumbnail, CardItem, Label } from 'native-base';
-import { Image, View, StyleSheet } from 'react-native';
+import { Image, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
 import * as Animatable from 'react-native-animatable';
-import styles, { MAX_HEIGHT, MIN_HEIGHT, optionsStyles } from './styles';
+import styles, { MAX_HEIGHT, MIN_HEIGHT, optionsStyles, sliderWidth, itemWidth } from './styles';
 import TimeAgo from 'react-native-timeago';
+import ImageLoad from 'react-native-image-placeholder';
+import Carousel from 'react-native-snap-carousel';
 import Menu, {
     MenuContext,
     MenuTrigger,
@@ -107,6 +109,133 @@ class ItemDetail extends Component {
         );
     }
 
+    _renderMetadata(item) {
+        if (item.metadata && item.metadata.image) {
+            return (
+                <CardItem style={{ paddingTop: 0 }}>
+                    <Left>
+                        <View style={styles.descLeftContainer} />
+                        <Body>
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                                style={styles.metaContainer}
+                                onPress={() => { alert(`You've clicked metadata`); }}>
+                                <View style={styles.imageContainer}>
+                                    <ImageLoad
+                                        placeholderSource={require('img/empty_image.png')}
+                                        source={{ uri: item.metadata.image }}
+                                        style={styles.image}
+                                    />
+                                </View>
+                                <View style={styles.textContainer}>
+                                    <Text style={styles.title} numberOfLines={2}>{item.metadata.title}</Text>
+                                    <Text style={styles.description} numberOfLines={2}>{item.metadata.description}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </Body>
+                    </Left>
+                </CardItem>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    _renderContext(entry) {
+        if (entry.type === "image") {
+            return (
+                <ImageLoad
+                    placeholderSource={require('img/empty_image.png')}
+                    source={{ uri: entry.imageSrc }}
+                    style={styles.image}
+                />
+            );
+        }
+        else if (entry.type === "video") {
+            var url = entry.text.toString();
+            var videoid = this.youtubeGetID(url);
+            if (Platform.OS === 'ios') {
+                return (
+                    <YouTube
+                        ref={(component) => {
+                            this._youTubeRef = component;
+                        }}
+                        apiKey={youTubeAPIKey}
+                        videoId={videoid}
+                        controls={1}
+                        style={styles.player}
+                    />
+                );
+            } else {
+                return (
+                    <WebView
+                        style={styles.player}
+                        javaScriptEnabled={true}
+                        source={{ uri: `https://www.youtube.com/embed/${videoid}?rel=0&autoplay=0&showinfo=0&controls=0` }}
+                    />
+                );
+            }
+        }
+        else {
+            return null;
+        }
+    }
+
+    _renderCarousel(item) {
+        if (item.poll) {
+            const slides = item.poll.educational_context.map((entry, index) => {
+                return (
+                    <TouchableOpacity
+                        key={`entry-${index}`}
+                        activeOpacity={0.7}
+                        style={styles.slideInnerContainer}
+                    >
+                        <View style={[styles.imageContainer, (index + 1) % 2 === 0 ? styles.imageContainerEven : {}]}>
+                            {this._renderContext(entry)}
+                            <View style={[styles.radiusMask, (index + 1) % 2 === 0 ? styles.radiusMaskEven : {}]} />
+                        </View>
+                    </TouchableOpacity>
+                );
+            });
+
+            return (
+                <CardItem cardBody>
+                    <Carousel
+                        sliderWidth={sliderWidth}
+                        itemWidth={itemWidth}
+                        inactiveSlideScale={1}
+                        inactiveSlideOpacity={1}
+                        enableMomentum={true}
+                        autoplay={false}
+                        autoplayDelay={500}
+                        autoplayInterval={2500}
+                        containerCustomStyle={styles.slider}
+                        contentContainerCustomStyle={styles.sliderContainer}
+                        showsHorizontalScrollIndicator={false}
+                        snapOnAndroid={true}
+                        removeClippedSubviews={false}
+                    >
+                        {slides}
+                    </Carousel>
+                </CardItem>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    _renderMedia(item) {
+        switch (item.entity.type) {
+            case 'post':
+            case 'user-petition':
+                return this._renderMetadata(item);
+                break;
+            default:
+                return this._renderCarousel(item);
+                break;
+        }
+    }
+
     render() {
         const { props: { item } } = this;
         return (
@@ -156,6 +285,8 @@ class ItemDetail extends Component {
                             {this._renderHeader(item)}
                         </TriggeringView>
                         {this._renderDescription(item)}
+                        {this._renderMedia(item)}
+                        <View style={{ height: 1000 }} />
                     </HeaderImageScrollView>
                 </Container>
             </MenuContext>
