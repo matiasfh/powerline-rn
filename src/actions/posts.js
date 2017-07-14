904d8f466fecc475019e131193ab6a705e604221
Awis A <awis0330@gmail.com>
2017-07-14 23:26:05 +0800

    issue #27 - fix bug in checking response of getting comments

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -51,7 +51,7 @@ async function loadPostComments(token: string, entityId: number, page: ?number =
             }
         });
         var json = await response.json();
-        if ((json.totalItems > 0) && (json.payload.length)) {
+        if (json && json.payload) {
             return Promise.resolve(json);
         }
         else {
